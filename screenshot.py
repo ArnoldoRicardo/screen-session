add1eba218f9d8d5dfa247d9ba8419ecc33110fb
ArnoldoRicardo <anyback@gmail.com>
2021-05-27 17:47:34 -0500

    update code for windows

--- screenshot.py
+++ screenshot.py
@@ -1,11 +1,20 @@
 import pyautogui
 import time
+import os
 import subprocess
 
+import os.path
 
-subprocess.call(['code'])
+script_dir = os.path.dirname(os.path.abspath(__file__))
+#im = Image.open)
+
+CHROME = os.path.join('C:\\', 'Program Files', 'Google', 'Chrome', 'Application', 'chrome.exe')
+#os.system('taskkill /im chrome.exe')
+subprocess.call([CHROME])
+# subprocess.call([CHROME, '--kiosk'])
+# subprocess.call(['code'])
 
 while(True):
     screenshot = pyautogui.screenshot()
-    screenshot.save(f"./log/log{time.asctime()}.png")
+    screenshot.save(f"{time.time()}.png")
     time.sleep(6)
